926df0b1e1b1d60d10ef1a75942af614754d9b12
James White <james@llamasantos.com>
2013-05-25 15:35:45 -0600

    New files

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express")
+    , app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use("html", require("hogan-express"));
+app.enable("view cache");
+
+app.get("/", function (req, res){
+    res.render("index", {partials : {}}); // nothing defined atm
+});
+
+app.listen(8080, function (err){
+    console.info("Started up on http://localhost:8080/");
+});
